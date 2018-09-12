8a07df79d6899e3ede7188811ee3fdce903ff247
chuber2 <chris.huber386@gmail.com>
2018-09-12 13:32:59 -0400

    Delete head.php

--- php/head.php
+++
@@ -1,6 +0,0 @@
-<head>
-    <meta charset="utf-8">
-		<title>Chris Huber, Cornell Student</title>
-		<!-- link to main stylesheet -->
-		<link rel="stylesheet" type="text/css" href="/css/main.css">
-</head>
